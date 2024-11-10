0881348cdb14d9a09e200fbee07ffab606a52498
Berg Macs <bergmacs@gmail.com>
2024-11-11 00:14:21 +0800

    remove things that I don't need yet: gen images by AI; yesterday quesions. And set get up early time for test.

--- get_up.py
+++ get_up.py
@@ -105,19 +105,22 @@ def make_get_up_message():
     now = pendulum.now(TIMEZONE)
     # 3 - 7 means early for me
     ###  make it to 9 in 2024.10.15 for maybe I forgot it ###
-    is_get_up_early = 3 <= now.hour <= 9
-    try:
-        images_list = make_pic_and_save(sentence)
-    except Exception as e:
-        print(str(e))
-        # give it a second chance
-        try:
-            sentence = get_one_sentence()
-            print(f"Second: {sentence}")
-            images_list = make_pic_and_save(sentence)
-        except Exception as e:
-            print(str(e))
-    return sentence, is_get_up_early, images_list
+    # Berg: 0 for test
+    is_get_up_early = 0 <= now.hour <= 9
+    # Berg: Do not gen images now.
+    # try:
+    #     images_list = make_pic_and_save(sentence)
+    # except Exception as e:
+    #     print(str(e))
+    #     # give it a second chance
+    #     try:
+    #         sentence = get_one_sentence()
+    #         print(f"Second: {sentence}")
+    #         images_list = make_pic_and_save(sentence)
+    #     except Exception as e:
+    #         print(str(e))
+    # return sentence, is_get_up_early, images_list
+    return sentence, is_get_up_early
 
 
 def get_yesterday_question():
@@ -152,33 +155,35 @@ def main(
     if is_today:
         print("Today I have recorded the wake up time")
         return
-    yesterday_question = get_yesterday_question()
-    sentence, is_get_up_early, images_list = make_get_up_message()
+    # yesterday_question = get_yesterday_question()
+    # sentence, is_get_up_early, images_list = make_get_up_message()
+    sentence, is_get_up_early = make_get_up_message()
     get_up_time = pendulum.now(TIMEZONE).to_datetime_string()
     body = GET_UP_MESSAGE_TEMPLATE.format(get_up_time=get_up_time, sentence=sentence)
     early_message = body
     if weather_message:
         weather_message = f"现在的天气是{weather_message}\n"
         body = weather_message + early_message
-    body = body + f"\n\n关于昨天的问题？\n{yesterday_question}"
+    # body = body + f"\n\n关于昨天的问题？\n{yesterday_question}"
     if is_get_up_early:
-        comment = body + f"![image]({images_list[0]})"
+        # comment = body + f"![image]({images_list[0]})"
+        comment = body
         issue.create_comment(comment)
         # send to telegram
-        if tele_token and tele_chat_id:
-            bot = telebot.TeleBot(tele_token)
+        # if tele_token and tele_chat_id:
+        #     bot = telebot.TeleBot(tele_token)
 
-            if images_list:
-                try:
-                    # sleep for waiting for the image to be generated
-                    time.sleep(4)
-                    photos_list = [InputMediaPhoto(i) for i in images_list[:4]]
-                    photos_list[0].caption = body
-                    bot.send_media_group(
-                        tele_chat_id, photos_list, disable_notification=True
-                    )
-                except Exception as e:
-                    print(str(e))
+        #     if images_list:
+        #         try:
+        #             # sleep for waiting for the image to be generated
+        #             time.sleep(4)
+        #             photos_list = [InputMediaPhoto(i) for i in images_list[:4]]
+        #             photos_list[0].caption = body
+        #             bot.send_media_group(
+        #                 tele_chat_id, photos_list, disable_notification=True
+        #             )
+        #         except Exception as e:
+        #             print(str(e))
     else:
         print("You wake up late")
 
