1fb12406c045a4835cf21d5c250c31c79393b74a
Berg Macs <62927379+Bergpss@users.noreply.github.com>
2024-11-22 23:08:31 +0800

    chore: Clean up get_up.py and update berg daily word message

--- get_up.py
+++ get_up.py
@@ -151,11 +151,11 @@ def main(
 ):
     u = login(github_token)
     repo = u.get_repo(repo_name)
-    issue = repo.get_issue(TEST_ISSUE_NUMBER)
+    issue = repo.get_issue(GET_UP_ISSUE_NUMBER)
     is_today = get_today_get_up_status(issue)
-    # if is_today:
-    #     print("Today I have recorded the wake up time")
-    #     return
+    if is_today:
+        print("Today I have recorded the wake up time")
+        return
     # yesterday_question = get_yesterday_question()
     # sentence, is_get_up_early, images_list = make_get_up_message()
     sentence, is_get_up_early = make_get_up_message()
@@ -166,7 +166,7 @@ def main(
         weather_message = f"现在的天气是{weather_message}\n"
         body = weather_message + early_message
     if berg_daily_word:
-        berg_daily_word = f"起床后的想法是：{berg_daily_word}\n"
+        berg_daily_word = f"起床后的想法是{berg_daily_word}\n"
         body = body + berg_daily_word
     # body = body + f"\n\n关于昨天的问题？\n{yesterday_question}"
     if is_get_up_early:
